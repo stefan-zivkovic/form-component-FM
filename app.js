eddbe9785d4feedff46c476bec1cb159c9dc0e5c
Stefan Živković <stefan55530@hotmail.com>
2022-01-23 17:01:38 +0100

    HTML & CSS done, next is JS

---
+++ app.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let firstName = "";
+  let lastName = "";
+  let email = "";
+  let password = "";
+});
+
+function validation(data) {
+  console.log(data);
+}
